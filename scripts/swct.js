fa767dd327eca7adba31f55749c2255fa41cb09e
Boguz <luis_bento@hotmail.com>
2016-10-06 09:28:30 +0200

    initial html

--- scripts/swct.js
+++ scripts/swct.js
@@ -1,4 +1,32 @@
+// DOCUMENT READY FUNCTION
 $(document).ready(function () {
 	
+	// CLICK ON STRIPE
+	$(".stripe").on("click", function() {
+		
+		closeStripe();
+		
+		openStripe( $(this) );
+		
+	});
+	
+	
 }); // end of document.ready
+
+
+// CLOSE OPEN STRIPE
+function closeStripe() {
+	
+	$(".open").removeClass("open");
+	
+}
+
+
+// OPEN CLICKED STRIPE
+function openStripe(stripe) {
+	
+	stripe.addClass("open");
+	
+}
+
 
